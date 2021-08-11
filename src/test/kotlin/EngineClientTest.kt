6e3c10983489cbd13e20c17f84573f25df4c906b
Christopher Zell <zelldon91@googlemail.com>
2021-08-11 14:13:13 +0200

    test: add deployment test

--- src/test/kotlin/EngineClientTest.kt
+++ src/test/kotlin/EngineClientTest.kt
@@ -1,5 +1,7 @@
 import io.camunda.zeebe.client.ZeebeClient
+import io.camunda.zeebe.model.bpmn.Bpmn
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.groups.Tuple
 import org.camunda.community.eze.EngineFactory
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -27,5 +29,35 @@ class EngineClientTest {
 
         // then
         assertThat(message.messageKey).isPositive;
+    }
+
+
+    @Test
+    fun shouldDeployProcess() {
+        // given
+        val zeebeClient = ZeebeClient.newClientBuilder().usePlaintext().build()
+
+        // when
+        val deployment = zeebeClient
+            .newDeployCommand()
+            .addProcessModel(
+                Bpmn.createExecutableProcess("simpleProcess")
+                    .startEvent()
+                    .endEvent()
+                    .done(),
+                "simpleProcess.bpmn")
+            .send()
+            .join()
+
+        // then
+        assertThat(deployment.key).isPositive;
+        assertThat(deployment.processes).isNotEmpty
+
+        val process = deployment.processes[0]
+
+        assertThat(process.version).isEqualTo(1)
+        assertThat(process.resourceName).isEqualTo("simpleProcess.bpmn")
+        assertThat(process.bpmnProcessId).isEqualTo("simpleProcess")
+        assertThat(process.processDefinitionKey).isPositive()
     }
 }
