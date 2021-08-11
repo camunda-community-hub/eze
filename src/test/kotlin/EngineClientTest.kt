db49863ab5f4cbcd28ff8b0207459c9ef22afa35
Christopher Zell <zelldon91@googlemail.com>
2021-08-11 13:39:37 +0200

    test: clean up

--- src/test/kotlin/EngineClientTest.kt
+++ src/test/kotlin/EngineClientTest.kt
@@ -1,24 +1,14 @@
 import io.camunda.zeebe.client.ZeebeClient
-import io.camunda.zeebe.gateway.protocol.GatewayGrpc
-import io.camunda.zeebe.gateway.protocol.GatewayOuterClass
-import io.grpc.Server
-import io.grpc.ServerBuilder
-import io.grpc.inprocess.InProcessChannelBuilder
-import io.grpc.inprocess.InProcessServerBuilder
-import io.grpc.stub.StreamObserver
-import io.grpc.util.MutableHandlerRegistry
 import org.assertj.core.api.Assertions.assertThat
 import org.camunda.community.eze.EngineFactory
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.util.*
 
 class EngineClientTest {
 
     @BeforeEach
     fun setupGrpcServer() {
-        val zeebeEngine = EngineFactory.create()
+        EngineFactory.create()
     }
 
 
